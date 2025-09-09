39d335c2b7c945994ee23f6242589523f18377c3
ShivaKarthick <shankaarshiva@gmail.com>
2025-09-09 23:27:03 +0800

    Accepted Leetcode 3195

--- leetcode/3195-Minimum-Area/main.py
+++ leetcode/3195-Minimum-Area/main.py
@@ -2,21 +2,28 @@ from typing import List
 
 
 def minimumArea(grid: List[List[int]]) -> int:
-    return 0
+    # Step 1: Shrinking the grid: Check if the top row, bottom row, left column and the right column are full of zeroes, if so remove them
+    while 1 not in grid[0]:
+        del grid[0]
+    while 1 not in grid[-1]:
+        del grid[-1]
+
+    # Transpose the grid, rows turn into columns and columns turn into rows
+    # create new transposed grid
+    grid_transposed = [
+        [grid[j][i] for j in range(len(grid))] for i in range(len(grid[0]))
+    ]
+
+    while 1 not in grid_transposed[0]:
+        del grid_transposed[0]
+    while 1 not in grid_transposed[-1]:
+        del grid_transposed[-1]
+
+    print(f"grid_transposed = {grid_transposed}")
+
+    return len(grid_transposed) * len(grid_transposed[0])
 
 
-grid = [[0,1,0],[1,0,1]]
+# grid = [[0, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 1], [1, 0, 0, 0], [0, 0, 0, 0]]
+grid = [[1, 0], [0, 0]]
 print(f"Minimum Area = {minimumArea(grid)}")
-
-
-def FastPower(a,b) :
-    if b == 1:
-        return a
-    c = a*a
-
-    ans = FastPower(c,[b/2])
-
-    if b % 2 == 1:
-        return a*ans
-    else:
-        return ans
