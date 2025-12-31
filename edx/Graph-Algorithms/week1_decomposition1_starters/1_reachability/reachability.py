5d4a1e46778a0bf6ff5cc1a6963a55e8cc40105b
SKRD <shankaarshiva@gmail.com>
2025-12-31 19:55:27 +0800

    Completed 1_reachability.py

--- edx/Graph-Algorithms/week1_decomposition1_starters/1_reachability/reachability.py
+++ edx/Graph-Algorithms/week1_decomposition1_starters/1_reachability/reachability.py
@@ -1,20 +1,41 @@
-import sys
-
 def reach(adj, x, y):
-    # write your code here
-    return 0
+    visited = [False] * n  # Only initialise once
+
+    def dfs(x):
+        visited[x] = True  # Mark the starting vertex as explored
+        for neighbour in adj[x]:
+            if neighbour == y:
+                return 1  # Found it immediately
+            if not visited[neighbour]:  # not False = True
+                if dfs(neighbour) == 1:
+                    return 1  # Found it deeper in the recursion
+        return 0  # Dead end
+
+    return dfs(x)
 
 
-if __name__ == "__main__":
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(data[0 : (2 * m) : 2], data[1 : (2 * m) : 2]))
-    x, y = data[2 * m :]
-    adj = [[] for _ in range(n)]
-    x, y = x - 1, y - 1
-    for a, b in edges:
-        adj[a - 1].append(b - 1)
-        adj[b - 1].append(a - 1)
-    print(reach(adj, x, y))
+# if __name__ == "__main__":
+inp1 = """
+4 4
+1 2
+3 2
+4 3
+1 4
+1 4
+"""
+
+input = inp1.strip()
+data = list(map(int, input.split()))
+n, m = data[
+    0:2
+]  # The first number is the number of vertices n and the second is the number of edges m
+data = data[2:]
+edges = list(zip(data[0 : (2 * m) : 2], data[1 : (2 * m) : 2]))
+x, y = data[2 * m :]  # Output 1 if there is a path between x and y and 0 otherwise
+adj = [[] for _ in range(n)]
+x, y = x - 1, y - 1
+for a, b in edges:
+    adj[a - 1].append(b - 1)
+    adj[b - 1].append(a - 1)
+
+print(reach(adj, x, y))
