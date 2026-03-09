7a32678b7bffd7c3fe73a43f46e50b82da700341
sk <shankaarshiva@gmail.com>
2026-03-09 23:24:08 +0800

    Refactor myPow method to handle negative exponents earlier and improve readability; update test values.

--- leetcode/0050/main.py
+++ leetcode/0050/main.py
@@ -1,10 +1,20 @@
+from functools import lru_cache
+
+
+@lru_cache
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         # Base Case
-        if n < 0:
-            n = abs(n)
         if n == 0:
             return 1
+        # Negative case
+        if n < 0:
+            n = abs(n)
+            half_power = self.myPow(x, n // 2)
+            if n % 2 == 0:
+                return 1 / (half_power * half_power)
+            elif n % 2 != 0:
+                return 1 / (half_power * half_power * x)
         half_power = self.myPow(x, n // 2)
         if n % 2 == 0:
             return half_power * half_power
@@ -13,6 +23,6 @@ class Solution:
 
 
 sol = Solution()
-x = 2.10000
-y = 3
+x = 343
+y = 576421
 print(sol.myPow(x, y))
