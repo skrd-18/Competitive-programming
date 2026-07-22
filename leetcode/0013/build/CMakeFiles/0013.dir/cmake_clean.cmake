243e6a93762dee7d1b15c4c6e83673d7f5046f89
sk <shankaarshiva@gmail.com>
2026-07-22 15:04:34 +0800

    build: ignore and untrack compiled binaries

---
+++ leetcode/0013/build/CMakeFiles/0013.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "0013"
+  "0013.pdb"
+  "CMakeFiles/0013.dir/main.cpp.o"
+  "CMakeFiles/0013.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/0013.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
